a1ef6100df58d3cb48b192455009921f5d27dc79
carlosgc-13 <gonzalezantonio_@hotmail.com>
2021-06-03 17:56:49 -0600

    new fields added to the tables

--- userAccount.sql
+++ userAccount.sql
@@ -2,5 +2,7 @@ use healthcare;
 
 create table userAccount(
 id int not null auto_increment,
-userName varchar(30) not null
+userName varchar(30) not null,
+firstname varchar(30) not null,
+lastname varchar(30) not null
 )
---
+++ userPassword.sql
@@ -0,0 +1,6 @@
+use healthcare;
+
+create table userPassword(
+id int not null auto_increment,
+passphrase varchar(256) not null
+)
