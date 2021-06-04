ab760cae0d875d817025b0c490a167f9d0684940
carlosgc-13 <gonzalezantonio_@hotmail.com>
2021-06-04 09:24:22 -0600

    New fields added to tables

--- userAccount.sql
+++ userAccount.sql
@@ -4,5 +4,7 @@ create table userAccount(
 id int not null auto_increment,
 userName varchar(30) not null,
 firstname varchar(30) not null,
-lastname varchar(30) not null
+lastname varchar(30) not null,
+email varchar(50) not null,
+phoneNumber varchar(14) null
 )
--- userPassword.sql
+++ userPassword.sql
@@ -2,5 +2,7 @@ use healthcare;
 
 create table userPassword(
 id int not null auto_increment,
-passphrase varchar(256) not null
+passphrase varchar(256) not null,
+userAccountId int not null,
+CONSTRAINT `fk_userAccount` FOREIGN KEY (`userAccountId`) REFERENCES `userAccount` (`id`)
 )
